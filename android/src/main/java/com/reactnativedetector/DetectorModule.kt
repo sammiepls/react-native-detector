e5fb7c05b044ee85af633be00e84960a92776b77
Samantha Lau <samantha.lhe@gmail.com>
2025-05-29 10:53:22 +0800

    fix: adds Android 14+ implementation and fixes native module cannot be null error

--- android/src/main/java/com/reactnativedetector/DetectorModule.kt
+++ android/src/main/java/com/reactnativedetector/DetectorModule.kt
@@ -4,29 +4,34 @@ import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
 import com.facebook.react.modules.core.DeviceEventManagerModule
-
-
+import android.app.Activity
 
 class DetectorModule(val reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext), ScreenshotDetectionListener {
-    private val screenshotDetectionDelegate = ScreenshotDetectionDelegate(reactContext, this)
+    private var screenshotDetectionDelegate: ScreenshotDetectionDelegate? = null
+
     override fun getName(): String {
         return "Detector"
     }
 
     @ReactMethod
     fun startScreenshotDetection() {
-        screenshotDetectionDelegate.startScreenshotDetection()
+        val currentActivity = reactContext.currentActivity
+        if (currentActivity != null) {
+            screenshotDetectionDelegate = ScreenshotDetectionDelegate(currentActivity, this)
+            screenshotDetectionDelegate?.startScreenshotDetection()
+        }
     }
 
     @ReactMethod
     fun stopScreenshotDetection() {
-        screenshotDetectionDelegate.stopScreenshotDetection()
+        screenshotDetectionDelegate?.stopScreenshotDetection()
+        screenshotDetectionDelegate = null;
     }
 
     override fun onScreenCaptured(path: String) {
         reactContext
-                .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter::class.java)
-                .emit("UIApplicationUserDidTakeScreenshotNotification", null)
+            .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter::class.java)
+            .emit("UIApplicationUserDidTakeScreenshotNotification", null)
     }
 
     override fun onScreenCapturedWithDeniedPermission() {
